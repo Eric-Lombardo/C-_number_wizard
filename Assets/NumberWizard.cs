cb225fb61111a72a96ee150a1445d399e77009c9
Eric Lombardo <ejlanim8or@gmail.com>
2020-04-27 21:33:09 -0400

    create git ignore

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+    int high = 2000;
+    int low = 1;
+    int guess = 500;
+
     // Start is called before the first frame update
     void Start()
     {
-        int high = 2000;
-        int low = 1;
 
         Debug.Log("Welcome to number wizard");
         Debug.Log("Pick a number but don't tell me what it is");
@@ -24,12 +26,19 @@ public class NumberWizard : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow)) 
         {
             Debug.Log("Up Arrow key was pressed.");
+            low = guess;
+            Debug.Log("the minimum is now: " + low );
+            Debug.Log("is your number " + guess + "?");
         } else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Debug.Log("Down Arrow was pressed");
+            high = guess;
+            Debug.Log("the maximum is now: " + high);
+            Debug.Log("is your number " + guess + "?");
         } else if (Input.GetKeyDown(KeyCode.Return)) 
         {
             Debug.Log("Return was pressed");
+            Debug.Log("The computer guessed your number!!");
         }
     }
 }
