05d95ec4a400e07c4b072cb7c42583aaad0bb26b
Eric Lombardo <ejlanim8or@gmail.com>
2020-04-27 22:04:53 -0400

    game complete

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,21 +4,14 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-    int high = 2000;
-    int low = 1;
-    int guess = 500;
+    int high;
+    int low;
+    int guess;
 
     // Start is called before the first frame update
     void Start()
     {
-
-        Debug.Log("Welcome to number wizard");
-        Debug.Log("Pick a number but don't tell me what it is");
-        Debug.Log("Highest number you can pick is " + high);
-        Debug.Log("Lowest number you can pick is " + low);
-        Debug.Log("Tell me if you're number is higher or lower than " + guess);
-        Debug.Log("Push up = higher, push down = lower, push enter = correct");
-        high++;
+        StartGame();
     }
 
     // Update is called once per frame
@@ -27,21 +20,35 @@ public class NumberWizard : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow)) 
         {
             low = guess;
-            guess = CalculateNewGuess(low, high);
-            Debug.Log("is your number " + guess + "?");
+            CalculateNewGuess();
         } else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             high = guess;
-            guess = CalculateNewGuess(low, high);
-            Debug.Log("is your number " + guess + "?");
+            CalculateNewGuess();
         } else if (Input.GetKeyDown(KeyCode.Return)) 
         {
             Debug.Log("The computer guessed your number!!");
+            StartGame();
         }
     }
 
-    int CalculateNewGuess(int low, int high) 
+    void CalculateNewGuess() 
     {
-        return (low + high) / 2;
+        guess = (low + high) / 2;
+        Debug.Log("is your number " + guess + "?");
+    }
+
+    void StartGame() 
+    {
+        high = 2000;
+        low = 1;
+        guess = 1000;
+        Debug.Log("Welcome to number wizard");
+        Debug.Log("Pick a number but don't tell me what it is");
+        Debug.Log("Highest number you can pick is " + high);
+        Debug.Log("Lowest number you can pick is " + low);
+        Debug.Log("Tell me if you're number is higher or lower than " + guess);
+        Debug.Log("Push up = higher, push down = lower, push enter = correct");
+        high++;
     }
 }
